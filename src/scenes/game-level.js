d519c73ead0eec5d79da3fa012080ccfcb9b67ea
dima <dima@uvision.co.il>
2025-02-17 12:49:26 +0200

    Add orcs

--- src/scenes/game-level.js
+++ src/scenes/game-level.js
@@ -13,11 +13,22 @@ export class GameLevel extends Scene {
 
         // this.stone = this.tiles.getSprite(9);
         // this.stone.setXY(10, 10);
-        this.orcTiles = new CharacterSheet({imageName: 'orc'});
 
-        this.orc = this.orcTiles.getAnimation('walk_right');
-       // this.orc = this.orcTiles.getAnimation([25,25,25], 300);
-        this.orc.setXY(10, 340);
+        this.playerTiles = new CharacterSheet({imageName: 'player'});
+        this.player = this.playerTiles.getAnimation('thrust_left');
+        this.player.setXY(353, 340);
+
+        this.orcTiles = new CharacterSheet({imageName: 'orc'});
+        this.orc = this.orcTiles.getAnimation('protection_right');
+        this.orc.setXY(310, 340);
+
+        this.orcTiles2 = new CharacterSheet({imageName: 'orc'});
+        this.orc2 = this.orcTiles2.getAnimation('walk_right');
+        this.orc2.setXY(10, 320);
+
+        this.orcTiles3 = new CharacterSheet({imageName: 'orc'});
+        this.orc3 = this.orcTiles3.getAnimation('walk_right');
+        this.orc3.setXY(80, 360);
     }
 
     init() {
@@ -27,6 +38,9 @@ export class GameLevel extends Scene {
     }
     update(time) {
         this.orc.update(time);
+        this.orc2.update(time);
+        this.orc3.update(time);
+        this.player.update(time);
     }
 
     render(time) {
@@ -35,6 +49,9 @@ export class GameLevel extends Scene {
         this.game.screen.drawSprite(this.map);
         //this.game.screen.drawSprite(this.stone);
         this.game.screen.drawSprite(this.orc);
+        this.game.screen.drawSprite(this.orc2);
+        this.game.screen.drawSprite(this.orc3);
+        this.game.screen.drawSprite(this.player);
         super.render(time);
     }
 }
